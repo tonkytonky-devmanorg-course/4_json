4342e661bbaa622934c6c2d68dd436f479df193b
tonkytonky <as@asamsonov.me>
2017-08-07 00:48:13 +0300

    Add general functionality

--- pprint_json.py
+++ pprint_json.py
@@ -1,13 +1,20 @@
 import json
+import sys
 
 
 def load_data(filepath):
-    pass
+    with open(filepath, encoding='utf-8') as input_file:
+        return json.load(input_file)
 
 
 def pretty_print_json(data):
-    pass
+    print(json.dumps(data, ensure_ascii=False, sort_keys=True, indent=4, separators=(',', ': ')))
+
+
+def main():
+    args = sys.argv[1:]
+    pretty_print_json(load_data(args[0]))
 
 
 if __name__ == '__main__':
-    pass
+    main()
