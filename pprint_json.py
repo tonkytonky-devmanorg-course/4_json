daedebedb777810007318477e7cc9db37051db74
tonkytonky <as@asamsonov.me>
2017-08-08 23:45:47 +0300

    Rename functions and variables with more understandable names

--- pprint_json.py
+++ pprint_json.py
@@ -2,18 +2,18 @@ import json
 import sys
 
 
-def load_data(filepath):
+def load_json(filepath):
     with open(filepath, encoding='utf-8') as input_file:
         return json.load(input_file)
 
 
-def pretty_print_json(data):
-    print(json.dumps(data, ensure_ascii=False, sort_keys=True, indent=4, separators=(',', ': ')))
+def pretty_print_json(loaded_json):
+    print(json.dumps(loaded_json, ensure_ascii=False, sort_keys=True, indent=4, separators=(',', ': ')))
 
 
 def main():
     args = sys.argv[1:]
-    pretty_print_json(load_data(args[0]))
+    pretty_print_json(load_json(args[0]))
 
 
 if __name__ == '__main__':
